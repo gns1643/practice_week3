b9d079ff7ac3c499e56605c493ce7200addd515a
gns1643 <gns1643@naver.com>
2025-03-19 17:47:51 +0900

    연습

---
+++ Project1/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello c world");
+            Console.ReadLine();
+        }
+    }
+    
+}
