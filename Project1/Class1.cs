00a23ae3e8284dd9a7867eb474080a8d2ffddd84
gns1643 <gns1643@naver.com>
2025-03-19 19:36:01 +0900

    ~클래스

--- Project1/Class1.cs
+++ Project1/Class1.cs
@@ -6,12 +6,34 @@ using System.Threading.Tasks;
 
 namespace Project1
 {
+    
     class Class1
     {
+        // 가변길이 매개변수
+        public int Add(params int[] nums) 
+        {
+            int sum = 0;
+            foreach(int i in nums)
+            {
+                sum += i;
+            }
+            return sum;
+        }
+        public double Add(double a=0, double b=0)
+        {
+            return a + b;
+        }
+        static public void Test_Add()
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine(class1.Add(3, 5, 6, 7));
+            Console.WriteLine(class1.Add(b: 2.3, a: 3.6)); //명명된 매개변수
+            Console.ReadLine();
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("hello c world222");
-            Console.ReadLine();
+            //Test_Add();
+            //Class2.Test_Human();
         }
     }
     
---
+++ Project1/Class2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    public class Human
+    {
+        public int Hp = 100;
+        public int Mp = 100;
+        public int Exp = 0;
+        public int Level = 1;
+        public Human(int h, int m, int e, int l)
+        {
+            Hp = h;
+            Mp = m;
+            Exp = e;
+            Level = l;
+        }
+        public Human()
+        {
+        }
+        public void Attacked(int dmg)
+        {
+            Hp -= dmg;
+            Console.WriteLine(dmg + "만큼의 데미지를 입었습니다.");
+            if (Hp <= 0)
+            {
+                Console.WriteLine("캐릭터가 사망하였습니다.");
+            }
+        }
+        public int getHp()
+        {
+            return Hp;
+        }
+    }
+    public class Class2
+    {
+        public static void Test_Human()
+        {
+            Human Jihoon = new Human(200, 50, 30, 5);
+            Jihoon.Attacked(100);
+            Console.WriteLine($"남은 hp는 {Jihoon.getHp()} 입니다");
+            Console.ReadLine();
+        }
+    }
+}
